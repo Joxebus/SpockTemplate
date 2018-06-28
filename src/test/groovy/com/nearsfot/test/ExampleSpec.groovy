2c9372173cbaf295796e262a5586dfc9ea3680dc
Omar Bautista <obautista@skytouchtechnology.com>
2018-06-27 22:22:39 -0700

    Add ExampleSpec

---
+++ src/test/groovy/com/nearsfot/test/ExampleSpec.groovy
@@ -0,0 +1,20 @@
+package com.nearsfot.test
+
+import spock.lang.Specification
+
+class ExampleSpec extends Specification{
+
+    def "Sum 2 numbers"(){
+        given: "Two numbers 4 and 7"
+        int a = 4
+        int b = 7
+
+        when:"Plus operation"
+
+        int c = a+b
+
+        then:"The result must be 11"
+
+        c == 11
+    }
+}
