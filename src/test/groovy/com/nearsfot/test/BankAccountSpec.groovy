5ef4cccf6e188ec505233944ba8809d36f38fc21
Omar Bautista <obautista@skytouchtechnology.com>
2018-06-27 23:54:51 -0700

    Example 2 add solution for BankAccount and new tests
    Add solution to:
    
    - An account should increment money on deposit
    - An account should decrement money on withdraw
    
    Add new tests to complete
    
    - AccountService cannot withdraw a quantity higher than the money in the account
    - An account and an accountService

--- src/test/groovy/com/nearsfot/test/BankAccountSpec.groovy
+++ src/test/groovy/com/nearsfot/test/BankAccountSpec.groovy
@@ -1,6 +1,7 @@
 package com.nearsfot.test
 
 import com.nearsoft.beans.BankAccount
+import com.nearsoft.exceptions.BankAccountException
 import com.nearsoft.services.BankAccountService
 import spock.lang.Specification
 
@@ -8,19 +9,44 @@ class BankAccountSpec extends Specification {
 
     def "An account should increment money on deposit"(){
         given: "An account and an accountService"
+        BankAccount bankAccount = new BankAccount(amount: 0)
+        BankAccountService service = new BankAccountService()
 
-        when: "When the service deposit in the account 100"
+        when: "When the service receive the account and 100"
+        service.deposit(bankAccount, 100)
 
         then: "The balance should be 100"
+        service.balance(bankAccount) == 100
 
     }
 
     def "An account should decrement money on withdraw"(){
         given: "An account with 100 and an accountService"
+        BankAccount bankAccount = new BankAccount(amount: 100)
+        BankAccountService service = new BankAccountService()
 
         when: "When the service withdraw from the account 50"
+        service.withdraw(bankAccount, 50)
 
         then: "The balance should be 50"
+        service.balance(bankAccount) == 50
+    }
+
+    def "AccountService cannot withdraw a quantity higher than the money in the account"(){
+        given: "An account with 50 and an accountService"
+
+        when: "When the service withdraw from the account 100"
+
+        then: "The service should throw a BankAccountException"
+
+    }
+
+    def "AccountService should call method getAmount"(){
+        given: "An account and an accountService"
+
+        when: "When the service receive the account and 100"
+
+        then: "The balance should be 100"
 
     }
 
