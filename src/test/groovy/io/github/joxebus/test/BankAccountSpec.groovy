207fc0c7a8372ad4ea603e4d039cbc98709dea03
Omar Bautista <joxebus@gmail.com>
2020-07-30 19:15:34 -0500

    Refactor to use the service as @Shared element

--- src/test/groovy/io/github/joxebus/test/BankAccountSpec.groovy
+++ src/test/groovy/io/github/joxebus/test/BankAccountSpec.groovy
@@ -2,14 +2,21 @@ package io.github.joxebus.test
 
 import io.github.joxebus.beans.BankAccount
 import io.github.joxebus.services.BankAccountService
+import spock.lang.Shared
 import spock.lang.Specification
 
 class BankAccountSpec extends Specification {
 
+    @Shared
+    BankAccountService service
+
+    def setupSpec() {
+        service = new BankAccountService()
+    }
+
     def "An account should increment money on deposit"(){
         given: "An account and an accountService"
         BankAccount bankAccount = new BankAccount(amount: 0)
-        BankAccountService service = new BankAccountService()
 
         when: "When the service receive the account and 100"
         service.deposit(bankAccount, 100.0)
@@ -22,7 +29,6 @@ class BankAccountSpec extends Specification {
     def "An account should decrement money on withdraw"(){
         given: "An account with 100 and an accountService"
         BankAccount bankAccount = new BankAccount(amount: 100.0)
-        BankAccountService service = new BankAccountService()
 
         when: "When the service withdraw from the account 50"
         service.withdraw(bankAccount, 50.0)
