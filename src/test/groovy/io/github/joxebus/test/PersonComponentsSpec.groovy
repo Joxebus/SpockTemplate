153ffce1853ed33f999735fee8f53a8fbb789a4b
Omar Bautista <joxebus@gmail.com>
2020-07-30 22:21:53 -0500

    Prepare template for test PersonComponentsSpec

--- src/test/groovy/io/github/joxebus/test/PersonComponentsSpec.groovy
+++ src/test/groovy/io/github/joxebus/test/PersonComponentsSpec.groovy
@@ -23,18 +23,11 @@ class PersonComponentsSpec extends Specification {
         )
 
         and: "setup service response creating a new person"
-        personService.create(name,_,age) >> new Person(name: name, age: age)
 
         when: "call the method create on controller with any parameter"
-        Person person = personController.create("something","something", 0)
 
         then:"validate the response object is the same as the created in the stubbed interaction"
 
-        person.with {
-            it.name == name
-            it.age == age
-            !phone
-        }
 
         where: "Information provided as data table with name and age"
         name    |   age
@@ -52,25 +45,19 @@ class PersonComponentsSpec extends Specification {
         )
 
         when: "when the method create is called on the controller"
-        personController.create("Omar", "423-132-2341", 32)
 
         then: "validate the interaction with the service and the parameters sent"
 
-        1 * personService.create("Omar", "423-132-2341", 32)
 
     }
 
     @Unroll("Testing exception is thrown with phone #phone and #age")
     def "Test exception with different data"(){
         given: "personService with a real implementation and a controller with that service"
-        personService = new PersonServiceImpl()
-        personController = new PersonController(personService: personService)
 
-        when: "controller recieves data to create a person with the wrong format phone"
-        Person person = personController.create(name, phone, age)
+        when: "controller receives data to create a person with the wrong format phone"
 
         then: "the service throws a RuntimeException"
-        thrown RuntimeException
 
         where: "Information provided as data pipes"
         name  << ["Sofia", "Oscar", "Omar"]
