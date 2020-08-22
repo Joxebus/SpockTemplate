c8f16f910286ea0ae905ed3586b218b6ccb8fff7
Omar Bautista <joxebus@gmail.com>
2020-08-22 01:50:28 -0500

    Solution for example4

--- src/test/groovy/io/github/joxebus/test/FeedReaderInteractionSpec.groovy
+++ src/test/groovy/io/github/joxebus/test/FeedReaderInteractionSpec.groovy
@@ -1,14 +1,8 @@
 package io.github.joxebus.test
 
-import io.github.joxebus.beans.BlogPost
-import io.github.joxebus.beans.Book
-import io.github.joxebus.beans.Feed
-import io.github.joxebus.service.BlogPostFeedService
-import io.github.joxebus.service.BookFeedService
 import io.github.joxebus.service.FeedService
 import io.github.joxebus.utils.FeedReaderGroovy
 import io.github.joxebus.utils.FeedReaderJava
-import spock.lang.Shared
 import spock.lang.Specification
 
 class FeedReaderInteractionSpec extends Specification {
@@ -16,40 +10,34 @@ class FeedReaderInteractionSpec extends Specification {
     String bookFeedUrl = "https://www.bookbrowse.com/rss/book_news.rss"
     String blogFeedUrl = "https://groovylopeando.wordpress.com/?feed=rss2"
 
-    FeedService<Book> bookFeedService;
-    FeedService<Feed> blogFeedService;
+    FeedService feedService
 
-    def "Test interaction between FeedReaderJava and bookFeedService"() {
-        given: "FeedReaderJava with a bookFeedService mock"
-
-        when: "readFeedRecords method from feedReader is called"
-
-        then: "extractFromFeed method from feedService should be called 1 time"
+    void setup() {
+        feedService = Mock()
     }
 
-    def "Test interaction between FeedReaderJava and blogFeedService"() {
-        given: "FeedReaderJava with a blogFeedService mock"
+    def "Test interaction between FeedReaderJava and feedService"() {
+        given: "FeedReaderJava with a feedService mock"
+        FeedReaderJava feedReader = new FeedReaderJava(feedService)
 
-        when: "readFeedRecords method from feedReader is called"
+        when: "readFeedRecords method from feedReader is called for both urls"
+        feedReader.readFeedRecords(bookFeedUrl)
+        feedReader.readFeedRecords(blogFeedUrl)
 
-        then: "extractFromFeed method from feedService should be called 1 time"
-
+        then: "extractFromFeed method from feedService should be called 2 times"
+        2 * feedService.extractFromFeed(_)
     }
 
-    def "Test interaction between FeedReaderGroovy bookFeedService"() {
-        given: "FeedReaderGroovy with a bookFeedService mock"
+    def "Test interaction between FeedReaderGroovy feedService"() {
+        given: "FeedReaderGroovy with a feedService mock"
+        FeedReaderGroovy feedReader = new FeedReaderGroovy(feedService)
 
-        when: "readFeedRecords method from feedReader is called"
+        when: "readFeedRecords method from feedReader is called for both urls"
+        feedReader.readFeedRecords(bookFeedUrl)
+        feedReader.readFeedRecords(blogFeedUrl)
 
-        then: "extractFromFeed method from feedService should be called 1 time"
-    }
-
-    def "Test interaction between FeedReaderGroovy blogFeedService"() {
-        given: "FeedReaderGroovy with a blogFeedService mock"
-
-        when: "readFeedRecords method from feedReader is called"
-
-        then: "extractFromFeed method from feedService should be called 1 time"
+        then: "extractFromFeed method from feedService should be called 2 times"
+        2 * feedService.extractFromFeed(_)
     }
 
 }
--- src/test/groovy/io/github/joxebus/test/FeedReaderSpec.groovy
+++ src/test/groovy/io/github/joxebus/test/FeedReaderSpec.groovy
@@ -2,7 +2,11 @@ package io.github.joxebus.test
 
 import io.github.joxebus.beans.Book
 import io.github.joxebus.beans.Feed
+import io.github.joxebus.service.BlogPostFeedService
+import io.github.joxebus.service.BookFeedService
 import io.github.joxebus.service.FeedService
+import io.github.joxebus.utils.FeedReaderGroovy
+import io.github.joxebus.utils.FeedReaderJava
 import spock.lang.Shared
 import spock.lang.Specification
 
@@ -11,42 +15,58 @@ class FeedReaderSpec extends Specification {
     String bookFeedUrl = "https://www.bookbrowse.com/rss/book_news.rss"
     String blogFeedUrl = "https://groovylopeando.wordpress.com/?feed=rss2"
 
+    @Shared
     FeedService<Book> bookFeedService;
+    @Shared
     FeedService<Feed> blogFeedService;
+
+    void setupSpec() {
+        bookFeedService = new BookFeedService()
+        blogFeedService = new BlogPostFeedService()
+    }
 
     def "FeedReaderGroovy with bookFeedService and can get 10 book records"() {
         given: "a FeedReaderGroovy with bookFeedService"
+        FeedReaderGroovy<Book> feedReader = new FeedReaderGroovy<>(bookFeedService)
 
         when: "feedReader read the bookFeedUrl"
+        List<Book> resultList = feedReader.readFeedRecords(bookFeedUrl)
 
         then: "the number of records returned are 10"
-
+        resultList.size() == 10
     }
 
     def "FeedReaderJava with bookFeedService and can get 10 book records"() {
         given: "a FeedReaderJava with bookFeedService"
+        FeedReaderJava<Book> feedReader = new FeedReaderJava<>(bookFeedService)
 
         when: "feedReader read the bookFeedUrl"
+        List<Book> resultList = feedReader.readFeedRecords(bookFeedUrl)
 
         then: "the number of records returned are 10"
-
+        resultList.size() == 10
     }
 
     def "FeedReaderGroovy with blogPostFeedService and can get 10 book records"() {
         given: "a FeedReaderGroovy with blogPostFeedService"
+        FeedReaderGroovy<Book> feedReader = new FeedReaderGroovy<>(blogFeedService)
 
-        when: "feedReader read the bookFeedUrl"
+        when: "feedReader read the blogFeedUrl"
+        List<Book> resultList = feedReader.readFeedRecords(blogFeedUrl)
 
         then: "the number of records returned are 10"
-
+        resultList.size() == 10
     }
 
     def "FeedReaderJava with blogPostFeedService and can get 10 book records"() {
         given: "a FeedReaderJava with blogPostFeedService"
+        FeedReaderJava<Book> feedReader = new FeedReaderJava<>(blogFeedService)
 
         when: "feedReader read the blogFeedUrl"
+        List<Book> resultList = feedReader.readFeedRecords(blogFeedUrl)
 
         then: "the number of records returned are 10"
+        resultList.size() == 10
 
     }
 
