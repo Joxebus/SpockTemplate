95967292da1844cae0f9c2df907ec2f38f210029
Omar Bautista <joxebus@gmail.com>
2026-02-06 23:34:58 -0600

    Upgrade source/target to Java 21, SpringBoot 3.2.3 and Spock 2.4-M4

--- src/main/java/io/github/joxebus/controller/PersonController.java
+++ src/main/java/io/github/joxebus/controller/PersonController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
